e9370b99ae8c69dd5a61c450a03b304427c1613d
Gabriel <gabrieltsf10@gmail.com>
2022-01-19 18:21:43 -0300

    Adding logic to allow writing data to csv files

--- filewrite.py
+++ filewrite.py
@@ -1,4 +1,5 @@
 from datetime import date
+from parse import ParseCsv
 import os
 import os.path
 
@@ -83,4 +84,44 @@ class FileWrite:
             print("*O arquivo já existe, o programa escreverá os novos conteúdos no final do arquivo original*")
             file = open(f'Estatística-Covid_{data_hoje}.json', 'a')
             file.write(data)
+            file.close()
+            
+    def FileWriteCsv(data):
+        
+        dia = date.today()
+        data_hoje = (f'{dia.day}.{dia.month}.{dia.year}')
+        username = os.getlogin()
+        folder = FolderWrite.FolderWrite()
+        if folder == 0:
+            path = (f'C:\\Users\\{username}\\Desktop\\CovidApiScript')
+        if folder == 1:
+            try:
+                path = (f'C:\\Users\\{username}\\Desktop\\CovidApiScript')
+            except:
+                path = (f'C:\\Users\\{username}\\Desktop\\CovidApiScript')
+
+        try:
+            file = open(f'{path}\\Estatística-Covid_{data_hoje}.csv', 'a')
+            file.write((f'Continente:,País,População:,Novos casos:,Casos ativos:,Casos críticos:,Recuperados:,Casos Totais:,Mortes Totais:,Mortes no período:\n'))
+            i = 0
+            gdata = ""
+            for x in data:
+                gdata += ParseCsv.ParseCountryData(data, i)
+                gdata += ParseCsv.ParseFirstData(data, i)
+                gdata += ParseCsv.ParseSecondData(data, i)
+                i+=1
+            file.write(gdata)
+            file.close()
+        except:
+            print("*O arquivo já existe, o programa escreverá os novos conteúdos no final do arquivo original*")
+            file = open(f'{path}\\Estatística-Covid_{data_hoje}.csv', 'a')
+            file.write((f'Continente:,País,População:,Novos casos:,Casos ativos:,Casos críticos:,Recuperados:,Casos Totais:,Mortes Totais:,Mortes no período:\n'))
+            i = 0
+            gdata = ""
+            for x in data:
+                gdata += ParseCsv.ParseCountryData(data, i)
+                gdata += ParseCsv.ParseFirstData(data, i)
+                gdata += ParseCsv.ParseSecondData(data, i)
+                i+=1
+            file.write(gdata)
             file.close()
